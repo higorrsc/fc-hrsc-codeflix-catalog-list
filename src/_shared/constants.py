12ffae3b1d424ad0da62e625d59a50e641965e93
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-05-27 20:22:43 -0300

    chore: add read environment variables to constants

--- src/_shared/constants.py
+++ src/_shared/constants.py
@@ -1,4 +1,7 @@
+import os
+
 DEFAULT_PAGINATION_SIZE = 5
 
-ELASTICSEARCH_HOST = "http://localhost:9200"
+ELASTICSEARCH_HOST = os.getenv("ELASTICSEARCH_HOST", "http://localhost:9200")
+ELASTICSEARCH_HOST_TEST = os.getenv("ELASTICSEARCH_TEST_HOST", "http://localhost:9201")
 ELASTICSEARCH_CATEGORY_INDEX = "catalog-db.codeflix.categories"
