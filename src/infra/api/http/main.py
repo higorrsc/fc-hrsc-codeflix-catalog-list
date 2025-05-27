934f8cad46ddca703ecbc3878cf19dbe04af3e92
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-05-27 19:46:47 -0300

    chore: add http api using fastapi

---
+++ src/infra/api/http/main.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from uuid import uuid4
+
+from fastapi import FastAPI
+
+from src.domain.category import Category
+
+app = FastAPI()
+
+movies_category = Category(
+    id=uuid4(),
+    name="Filme",
+    description="Categoria de filmes",
+    created_at=datetime.now(),
+    updated_at=datetime.now(),
+    is_active=True,
+)
+
+series_category = Category(
+    id=uuid4(),
+    name="Série",
+    description="Categoria de séries",
+    created_at=datetime.now(),
+    updated_at=datetime.now(),
+    is_active=True,
+)
+
+
+@app.get("/categories")
+def list_categories():
+    return {
+        "categories": [
+            movies_category,
+            series_category,
+        ]
+    }
